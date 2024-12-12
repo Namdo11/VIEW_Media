05b7b29f25cf72ae6405c46940e717cc90c94d43
namdh55 <namdao253200@gmail.com>
2024-12-12 22:15:11 +0700

    Hi_add int d

--- Nam_au15_2/src/main.cpp
+++ Nam_au15_2/src/main.cpp
@@ -19,6 +19,7 @@ void musicThread(MusicController& controller) {
 int a = 10;
 int b = 20;
 int c = 30;
+int d = 40;
 int main() {
     // Khởi tạo SDL và SDL_image một lần trước vòng lặp chính
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
